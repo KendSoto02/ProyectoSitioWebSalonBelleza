0d5a5e4cd6a5883facc86f06415bd5705bc7feed
KendSoto02 <sotofajardo05@gmail.com>
2023-06-07 15:14:48 -0600

    endPoints-added

--- Servidor/src/server.js
+++ Servidor/src/server.js
@@ -60,7 +60,6 @@ app.post('/enviar-datos', (req, res) => {
       request.input('NombreCliente', sql.NVarChar(50), nombre);
       request.input('MedioContactoCliente', sql.NVarChar(50), telefono);
       request.input('FechaHora', sql.DateTime, fechaHora);
-      console.log(servicio)
       request.execute('AgendarCita', (err, result) => {
         if (err) {
           console.log(err);
@@ -72,6 +71,88 @@ app.post('/enviar-datos', (req, res) => {
     }
   });
 });
+
+app.delete('/eliminar-reservacion', (req, res) => {
+  // Obtener los datos enviados en la solicitud
+  const { IdCita } = req.body;
+
+  // Conectar a la base de datos y llamar al stored procedure
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      request.input('IdCita', sql.Int, IdCita);
+     
+      request.execute('eliminarCita', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('La cita fue eliminada correctamente');
+        }
+      });
+    }
+  });
+});
+
+app.delete('/eliminar-servicio/:id', (req, res) => {
+  // Obtener el IdServicio de los parámetros de la URL
+  const IdServicio = req.params.id;
+
+  // Conectar a la base de datos y llamar al stored procedure
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      request.input('IdServicio', sql.Int, IdServicio);
+     
+      request.execute('eliminarServicio', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('El servicio fue eliminado correctamente');
+        }
+      });
+    }
+  });
+});
+
+
+app.delete('/eliminar-producto/:id', (req, res) => {
+  // Obtener el IdServicio de los parámetros de la URL
+  const idProducto = req.params.id;
+
+  // Conectar a la base de datos y llamar al stored procedure
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      request.input('idProducto', sql.Int, idProducto);
+     
+      request.execute('eliminarProducto', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('El producto fue eliminado correctamente');
+        }
+      });
+    }
+  });
+});
+
+
+
+
+
+
 
 
 // Ruta para manejar las solicitudes GET
@@ -97,10 +178,55 @@ app.get('/obtener-datos', (req, res) => {
 });
 
 
-app.post('/insertar-datos', (req, res) => {
+// Ruta para manejar las solicitudes GET para servicios
+app.get('/obtener-servicio', (req, res) => {
+  // Conectar a la base de datos y obtener los datos
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      const query = 'EXEC GetServicios';  // Llamada al procedimiento almacenado
+      request.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).json(result.recordset);
+        }
+      });
+    }
+  });
+});
+
+
+app.get('/ObtenerProductos', (req, res) => {
+  // Conectar a la base de datos y obtener los datos
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      const query = 'EXEC ObtenerProductos';  // Llamada al procedimiento almacenado
+      request.query(query, (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).json(result.recordset);
+        }
+      });
+    }
+  });
+});
+
+
+app.post('/insertar-servicios', (req, res) => {
   // Obtener los datos del cuerpo de la solicitud
-  //const datos = req.body;
-  const { imagen, descripcion, precio } = req.body;
+  const { descripcion, precio, FotoLink } = req.body;
+
   // Conectar a la base de datos y guardar los datos
   sql.connect(config, err => {
     if (err) {
@@ -112,6 +238,7 @@ app.post('/insertar-datos', (req, res) => {
       // Configurar los parámetros del stored procedure
       request.input('Descripcion', sql.NVarChar(100), descripcion);
       request.input('Precio', sql.Money, precio);
+      request.input('FotoLink', sql.NVarChar(sql.MAX), FotoLink);
 
       // Ejecutar el stored procedure
       request.execute('InsertarServicio', (err, result) => {
@@ -127,8 +254,140 @@ app.post('/insertar-datos', (req, res) => {
 });
 
 
+app.post('/insertar-productos', (req, res) => {
+  // Obtener los datos del cuerpo de la solicitud
+  const { NombreProducto, Descripcion, Foto, Precio } = req.body;
+
+  // Conectar a la base de datos y guardar los datos
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+
+      // Configurar los parámetros del stored procedure
+      request.input('NombreProducto', sql.NVarChar(200), NombreProducto);
+      request.input('Descripcion', sql.NVarChar(200), Descripcion);
+      request.input('Foto', sql.NVarChar(sql.MAX), Foto);
+      request.input('Precio', sql.Money, Precio);
+
+      // Ejecutar el stored procedure
+      request.execute('InsertarProducto', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('Se ha guardado el producto');
+        }
+      });
+    }
+  });
+});
+
+app.put('/editar-reservacion/:id', (req, res) => {
+  // Obtener el ID de la reservación a actualizar
+  const reservarCitaID = req.params.id;
+
+  // Obtener los datos enviados en la solicitud
+  const { nombre, telefono, fechaHora, servicio, producto } = req.body;
+
+  // Conectar a la base de datos y llamar al stored procedure
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      request.input('ReservarCitaID', sql.Int, reservarCitaID);
+      request.input('servicio', sql.NVarChar(50), servicio);
+      request.input('producto', sql.NVarChar(50), producto);
+      request.input('NombreCliente', sql.NVarChar(50), nombre);
+      request.input('MedioContactoCliente', sql.Int, telefono);
+      request.input('FechaHora', sql.DateTime, fechaHora);
+      request.execute('EditarReservacion', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('La reservación fue actualizada correctamente');
+        }
+      });
+    }
+  });
+});
+
+
+app.put('/editar-producto/:id', (req, res) => {
+  // Obtener el ID del producto a actualizar
+  const productoID = req.params.id;
+
+  // Obtener los datos enviados en la solicitud
+  const { nombreProducto, descripcion, foto, precio } = req.body;
+
+  // Conectar a la base de datos y llamar al stored procedure
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      request.input('ProductoID', sql.Int, productoID);
+      request.input('NombreProducto', sql.NVarChar(200), nombreProducto);
+      request.input('Descripcion', sql.NVarChar(200), descripcion);
+      request.input('Foto', sql.NVarChar(sql.MAX), foto);
+      request.input('Precio', sql.Money, precio);
+      request.execute('ActualizarProducto', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('El producto fue actualizado correctamente');
+        }
+      });
+    }
+  });
+});
+
+
+app.put('/editar-servicio/:id', (req, res) => {
+  // Obtener el ID del producto a actualizar
+  const ServicioID = req.params.id;
+
+  // Obtener los datos enviados en la solicitud
+  const { descripcion, precio, FotoLink } = req.body;
+
+  // Conectar a la base de datos y llamar al stored procedure
+  console.log(FotoLink)
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error de servidor');
+    } else {
+      const request = new sql.Request();
+      request.input('ServicioID', sql.Int, ServicioID);
+      request.input('Descripcion', sql.NVarChar(100), descripcion);
+      request.input('Precio', sql.Money, precio);
+      request.input('FotoLink', sql.NVarChar(sql.MAX), FotoLink);
+
+      request.execute('actualizarServicio', (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error de servidor');
+        } else {
+          res.status(200).send('El servicio fue actualizado correctamente');
+        }
+      });
+    }
+  });
+});
+
 
 
 // Iniciar el servidor Node.js
 const port = 3001;
 app.listen(port, () => console.log(`Servidor escuchando en el puerto ${port}`));
+
+
+
+
